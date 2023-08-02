5b5e10857b49ed58d201fce8d41ca4e98532eb5f
joaquincaggiano <joaquincaggiano@gmail.com>
2023-08-02 13:34:03 -0300

    inicializacion moviesapp

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  )
+}
+
+export default App
