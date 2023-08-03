6a6c6df44b3894910e7f62dca7516e34dc202396
joaquincaggiano <joaquincaggiano@gmail.com>
2023-08-03 12:47:32 -0300

    fix moviesDB

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Movie, MoviesResponse} from '../interfaces/movieInterfaces';
 import axios from 'axios';
+import moviesDB from '../api/moviesDB';
 
 export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -12,7 +13,7 @@ export const useMovies = () => {
   }, []);
 
   const getMovies = async () => {
-    const resp = await axios.get<MoviesResponse>(`https://api.themoviedb.org/3/movie/now_playing?page=${page}&api_key=29ae7ede5815710001d04a89c2736fa2&language=es-ES`);
+    const resp = await moviesDB.get<MoviesResponse>(`/now_playing?page=${page}`);
     setPage(page => page + 1)
     setMoviesInCine([...moviesInCine, ...resp.data.results]);
     setIsLoading(false);
@@ -24,32 +25,3 @@ export const useMovies = () => {
     getMovies
   };
 };
-
-// const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
-
-//   useEffect(() => {
-//     loadPokemons();
-//   }, []);
-
-//   const loadPokemons = async () => {
-//     setIsLoading(true);
-//     const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
-//     nextPageUrl.current = resp.data.next;
-//     mapPokemonList(resp.data.results);
-//   };
-
-//   const mapPokemonList = (pokemonList: Result[]) => {
-//     const newPokemonList: SimplePokemon[] = pokemonList.map(({name, url}) => {
-//       const urlParts = url.split('/');
-//       const id = urlParts[urlParts.length - 2];
-//       const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-//       return {
-//         id,
-//         picture,
-//         name,
-//       };
-//     });
-
-//     setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
-//     setIsLoading(false);
-//   };
